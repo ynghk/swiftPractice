7210e67cf10faf153ca88832a32b8c38e4626b6c
YNGHK <celebi3@naver.com>
2025-06-24 23:35:48 +0900

    Restricted TextField Practice

---
+++ RestrictedTF/RestrictedTF/RestrictedTFApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestrictedTFApp.swift
+//  RestrictedTF
+//
+//  Created by Yung Hak Lee on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct RestrictedTFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
